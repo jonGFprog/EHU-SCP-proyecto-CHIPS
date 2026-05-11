c72bb58177b973aeefe7228cc07708a14f909f3b
Jon Gonzalez <jongf2005@gmail.com>
2026-05-11 23:11:19 +0200

    primera version fase2, falta reduce de las configuraciones

---
+++ paralelo/fase2/diffusion_p.h
@@ -0,0 +1,3 @@
+/* File: diffusion.h */
+
+extern double calculate_Tmean (struct info_param param, float *grid, float *grid_chips, float *grid_aux, int tam_loc, int pid, int npr, MPI_Status info,MPI_Comm grupos);
